96491e5ee50e3070d07522ebd123be03ceb5c362
hinanaz87 <119865555+hinanaz87@users.noreply.github.com>
2023-03-12 23:04:07 +0500

    cors issue

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const app = express()
 app.use(express.json());
 // app.use(cors());
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://odd-gold-squid-robe.cyclic.app', "*"],
-  credentials: true
+  origin: ['http://localhost:3000', 'https://odd-gold-squid-robe.cyclic.app',"https://hackathon-hina-naz.netlify.app", "*"],
+  
 }));
 
 const PORT = process.env.PORT || 5000;
